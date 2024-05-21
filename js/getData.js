5408480efd486d660bc5fe6d0bee08509cc8af84
AdCa09 <caufriezadrien0@gmail.com>
2024-05-21 16:53:06 +0200

    add attendees.js need to finish

--- js/getData.js
+++ js/getData.js
@@ -1,12 +1,22 @@
 export const Data= async () => {
     const response = await fetch("http://localhost:3000/api/events",{
-        method: 'GET',
+        method: "GET",
         headers: {
-            "Content-Type": 'application/json',
+            "Content-Type": "application/json",
         },
+
     });
+
+
+
     const data = await response.json();
     return data;
 }
-Data();
+Data()
+ .then(data =>{
+    console.log(data);
+})
+.catch(error =>{
+    console.log("Une erreur s'est produite", error);
+})
 
